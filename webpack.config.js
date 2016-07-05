2eae88f9a86fd700c6975e2e78c2f6df40b046d1
Cheton Wu <cheton@gmail.com>
2016-07-05 16:54:40 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,49 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './main.js',
-  output: { path: __dirname, filename: 'bundle.js' },
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  },
+    entry: './main.js',
+    output: { path: __dirname, filename: 'bundle.js' },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'stage-0', 'react']
+                }
+            },
+            {
+                test: /\.styl$/,
+                loader: 'style!css!stylus'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css'
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url',
+                query: {
+                    limit: 8192
+                }
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url',
+                query: {
+                    limit: 10000,
+                    mimetype: 'application/font-woff'
+                }
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file'
+            }
+        ]
+    },
+    resolve: {
+        alias: {},
+        extensions: ['', '.js', '.jsx']
+    }
 };
